2ba2a2974bc240f3d53eb45808177c5cc5a359ec
Dwilhite27 <wilhitedurrelle@gmail.com>
2021-07-02 09:05:10 -0500

    Employee-Engineer-Tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,17 +1,21 @@
 const Engineer = require('../lib/Engineer');
 jest.mock('../lib/Intern');
-console.log(new Engineer);
 
-const Engineer = require('../lib/Engineer');
 
 test('checks to see if Engineer name is truthy', () => {
-    const engineer = new Engineer ('Durrelle');
-const expectedOfficeNumber = 10;
+    const engineer = new Engineer('Durrelle');
 
     expect(engineer.name).toBe('Durrelle');
-    expect(engineer.email).toBe(expect.any(String));
-    expect(engineer.id).toStrictEqual(expect.any(Number));
-    expect(engineer.officeNumber).toBe(expectedOfficeNumber);
-    expect(engineer.age).toBe(expect.any(Number));
+})
+test('can get the engineer email address', () => {
+    const engineer = new Engineer('Wilhitedurrelle@gmail.com');
+
+    expect(engineer.email).toBe(String);
+})
+test('can get the engineer id number', () => {
+    const engineerId = 100;
+    const engineer = new Engineer('Wilhite', engineerId);
+
+    expect(engineer.id).toBe(engineerId);
 })
 
