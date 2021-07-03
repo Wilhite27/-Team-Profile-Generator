ac84c98e010710cc64491a65163142641baa15c9
Dwilhite27 <wilhitedurrelle@gmail.com>
2021-07-03 01:33:45 -0500

    Finished-tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -8,16 +8,16 @@ test('checks to see if Engineer name is truthy', () => {
     expect(engineer.name).toBe('Durrelle');
 })
 test('checks to see if Engineer Github is truthy', () => {
-    const engineer = new Engineer('Durrelle');
+    const engineer = new Engineer('Durrelle', 100, 'Wilhitedurrelle@gmail.com.', 'Wilhite27');
 
     expect(engineer.github).toBe('Wilhite27');
 })
 
 
 test('can get the engineer email address', () => {
-    const engineer = new Engineer('Durrelle',  100,'Wislhitedurrelle@gmail.com', 'Wilhite27');
+    const engineer = new Engineer('Durrelle',  100,'Wilhitedurrelle@gmail.com', 'Wilhite27');
 
-    expect(engineer.email).toBe(String);
+    expect(engineer.email).toBe('Wilhitedurrelle@gmail.com');
 })
 test('can get the engineer id number', () => {
     const engineerId = 100;
